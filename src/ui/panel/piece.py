75ab352a00ebd6d0830a7da42fe6f7c827035fef
halak0013 <zumruduanka0013@gmail.com>
2025-02-27 15:57:23 +0300

    fixed math problem

--- src/ui/panel/piece.py
+++ src/ui/panel/piece.py
@@ -1,36 +1,40 @@
 from typing import List, Tuple
 from PyQt6.QtCore import QPoint
+from PyQt6.QtGui import QPolygon
 
-from static.config import Configs as cfg
+from src.static.config import Configs as cfg
 import math
 
 
 class Piece:
-    def __init__(self, layer_index, piece_index, center: Tuple[int, int] = (0, 0)):
-        self.coordiante_list: List[QPoint] = []
+    def __init__(self, layer_index, piece_index):
+        self.coordinate_list: List[QPoint] = []
         self.layer_index: int = layer_index
         self.piece_index: int = piece_index
         self.k: int = 2
         self.angle_spacing = 1
 
-    def _get_cooirdante(self, step: int, is_innener) -> Tuple[int, int]:
-        """
-        step -> yay çizilirken her bir adım \n
-        is_inner -> parçanın iç mi yoksa dış yayı mı çizilecek\n
-        merkez = (ş, e)\n
-        başlangıç noktası = (ş + (cos((p-1)\*a) / k\*r), e + (sin((p-1)\*a) / k\*r))
-        """
+        self._set_coordinate()
+
+    def _get_coordinate(self, step: int, is_inner) -> Tuple[int, int]:
+
         x, y = cfg.center
-        inner = 1 if is_innener else 0
+        inner = 1 if is_inner else 0
         radian = math.radians((self.piece_index - inner) * cfg.piece_angle + step)
         radius = self.k * ((self.piece_index - inner) * cfg.piece_radius)
-        return (x + (math.cos(radian) / radius), y + (math.sin(radian) / radius))
+        return (
+            int(x + (math.cos(radian) * radius)),
+            int(y + (math.sin(radian) * radius)),
+        )
 
     def _set_coordinate(self):
         for s in range(cfg.piece_angle):
-            self.coordiante_list.append(QPoint(self.get_cooirdante(s, True)))
-        
+            coord = self._get_coordinate(s, True)
+            self.coordinate_list.append(QPoint(coord[0], coord[1]))
+
         for s in range(cfg.piece_angle):
-            self.coordiante_list.append(QPoint(self.get_cooirdante(s, False)))
+            coord = self._get_coordinate(s, False)
+            self.coordinate_list.append(QPoint(coord[0], coord[1]))
 
-
+    def get_poligon(self) -> QPolygon:
+        return QPolygon(self.coordinate_list)
